cf20a8fc593590aa7749df2fd71eabc0f11976a6
Bilal Ahmed <hello@liveoakhome.co>
2025-03-23 21:18:24 -0700

    spring security ui

--- hooslyApp/grails-app/conf/application.groovy
+++ hooslyApp/grails-app/conf/application.groovy
@@ -4,6 +4,7 @@
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'security.User'
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'security.UserRole'
 grails.plugin.springsecurity.authority.className = 'security.Role'
+grails.plugin.springsecurity.ui.register.requireEmailValidation = false
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/',               access: ['permitAll']],
 	[pattern: '/error',          access: ['permitAll']],
